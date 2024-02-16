d3b5c29be423c67d409d0bcacc84b569abf9aecd
Okunade Olaitan <olaokunadeola@gmail.com>
2024-02-16 13:20:24 +0100

    clan socket

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
-    // methods: ["GET", "POST"],
+    methods: ["GET", "POST"],
   },
 });
 
